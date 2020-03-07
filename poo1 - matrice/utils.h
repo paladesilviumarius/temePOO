fd449c9f35d853daf0f9e0e660a4f91a7301fb73
smitoi <smitoi@users.noreply.github.com>
2020-03-08 00:53:06 +0200

    Adăugat fișierele într-un folder pentru primul proiect

---
+++ poo1 - matrice/utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <iostream> // INPUT / OUTPUT
+#include <time.h> // PENTRU RANDOM
+#include <stdlib.h> // PENTRU RANDOM
+#include "matrice.h" // MATRICE
+#include <cmath>
+
+matrice     randomMatrix(int, int, int);
+#endif // UTILS_H
